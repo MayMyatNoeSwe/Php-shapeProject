4f0d4bd7c502360fdd5348fdc70a012b75230f3d
MayMyatNoeSwe <myatnoen395@gmail.com>
2024-11-11 17:42:42 +0630

    PHP-ShapeProject

---
+++ shapel-html/index.php
@@ -0,0 +1,96 @@
+<!-- header section strats -->
+<?php
+include './layouts/header.php';
+
+
+?>
+<!-- end header section -->
+
+<!-- slider section -->
+<?php
+include './layouts/slider.php';
+
+
+?>
+<!-- end slider section -->
+
+<!-- about section -->
+
+<?php
+include './layouts/about.php';
+
+
+?>
+<!-- end about section -->
+
+<!-- gallery section -->
+<?php
+include './layouts/gallery.php';
+
+
+?>
+
+<!-- end gallery section -->
+
+
+<!-- service section -->
+<?php
+include './layouts/service.php';
+
+
+?>
+
+
+<!-- end service section -->
+
+
+
+<!-- blog section -->
+<?php
+include './layouts/blog.php';
+
+
+?>
+
+
+<!-- end blog section -->
+
+<!-- client section -->
+
+<?php
+include './layouts/client.php';
+
+
+?>
+<!-- end client section -->
+
+<!-- contact section -->
+<?php
+include './layouts/contact.php';
+
+
+?>
+<!-- end contact section -->
+
+<!-- info section -->
+<?php
+include './layouts/info.php';
+
+
+?>
+
+<!-- end info_section -->
+<!-- client section -->
+
+<?php
+include './layouts/client.php';
+
+
+?>
+
+<!-- footer section -->
+<?php include './layouts/footer.php'; ?>
+<!-- footer section -->
+
+
+<!-- jQery -->
---
+++ shapel-html/layouts/header.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link rel="icon" href="images/favicon.png" type="image/gif" />
+
+    <title>Shapel</title>
+
+    <!-- bootstrap core css -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+    <!-- fonts style -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap" rel="stylesheet" />
+
+    <!-- lightbox Gallery-->
+    <link rel="stylesheet" href="css/ekko-lightbox.css" />
+
+    <!-- font awesome style -->
+    <link href="css/font-awesome.min.css" rel="stylesheet" />
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet" />
+    <!-- responsive style -->
+    <link href="css/responsive.css" rel="stylesheet" />
+
+</head>
+
+<body>
+    <header class="header_section">
+        <div class="container-fluid">
+            <nav class="navbar navbar-expand-lg custom_nav-container">
+                <a class="navbar-brand" href="index.html">
+                    <span>
+                        Shapel
+                    </span>
+                </a>
+                <div class="" id="">
+
+                    <div class="custom_menu-btn">
+                        <button onclick="openNav()">
+                            <span class="s-1"> </span>
+                            <span class="s-2"> </span>
+                            <span class="s-3"> </span>
+                        </button>
+                        <div id="myNav" class="overlay">
+                            <div class="overlay-content">
+                                <a href="index.html">Home</a>
+                                <a href="about.html">About</a>
+                                <a href="gallery.html">Gallery</a>
+                                <a href="service.html">Service</a>
+                                <a href="blog.html">Blog</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
